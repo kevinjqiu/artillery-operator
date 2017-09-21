f4f45861bf4589920902dda5128615abc82f65bb
Kevin Jing Qiu <kevin@idempotent.ca>
2017-09-21 17:20:36 -0400

    Port k8sutil.NewClusterConfig

---
+++ pkg/k8sutil/k8sutil.go
@@ -0,0 +1,34 @@
+package k8sutil
+
+import (
+	"fmt"
+	"net/url"
+
+	"k8s.io/client-go/rest"
+)
+
+func NewClusterConfig(host string, tlsInsecure bool, tlsConfig *rest.TLSClientConfig) (*rest.Config, error) {
+	var cfg *rest.Config
+	var err error
+
+	if len(host) == 0 {
+		if cfg, err = rest.InClusterConfig(); err != nil {
+			return nil, err
+		}
+	} else {
+		cfg = &rest.Config{
+			Host: host,
+		}
+		hostURL, err := url.Parse(host)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing host url %s : %v", host, err)
+		}
+		if hostURL.Scheme == "https" {
+			cfg.TLSClientConfig = *tlsConfig
+			cfg.Insecure = tlsInsecure
+		}
+	}
+	cfg.QPS = 100
+	cfg.Burst = 100
+	return cfg, nil
+}
