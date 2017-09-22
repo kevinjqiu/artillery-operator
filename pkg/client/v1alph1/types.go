38a1e48ea5c7fdd31cfd3f218da53b812d00e0af
Kevin Jing Qiu <kevin@idempotent.ca>
2017-09-22 14:49:46 -0400

    Add apimachinery

---
+++ pkg/client/v1alph1/types.go
@@ -0,0 +1,25 @@
+package v1alph1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type Artillery struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	Spec              ArtillerySpec    `json:"spec"`
+	Status            *ArtilleryStatus `json:"status,omitempty"`
+}
+
+type ArtillerySpec struct {
+	BaseImage string `json:"baseImage,omitempty"`
+}
+
+type ArtilleryStatus struct {
+}
+
+type ArtilleryList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []*Artillery `json:"items"`
+}
